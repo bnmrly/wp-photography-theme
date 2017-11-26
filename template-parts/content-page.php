1cf36a56a2c9e022659356ef929959e9471fda9e
Ben Marley <benjaminjohnmarley@gmail.com>
2017-11-26 19:59:05 +0000

    Clean indentation

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,15 +10,14 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="entry-content">
+		<?php
+			the_content();
 
-		<div class="entry-content">
-			<?php
-				the_content();
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bianco_nero' ),
-					'after'  => '</div>',
-				) );
-			?>
-		</div><!-- .entry-content -->
-		</article><!-- #post-## -->
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bianco_nero' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
