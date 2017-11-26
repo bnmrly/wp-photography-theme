aa2b59aeb285028c34509a153bc93c9a5aa3138c
Ben Marley <benjaminjohnmarley@gmail.com>
2017-11-26 19:56:45 +0000

    Remove .entry-header comment

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,18 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<!-- BM CHANGE Remove .entry-header -->
-
-	<!-- <header class="entry-header">
-		 <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( // get_permalink() ) ), '</a></h2>' ); ?>
-
-		 <?php if ( 'post' === get_post_type() ) : ?>
-		 <div class="entry-meta">
-			<?php bianco_nero_posted_on(); ?> -->
-		</div>< <!-- .entry-meta -->
-		<?php endif; ?>
-	</header> <!-- .entry-header -->
-
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
