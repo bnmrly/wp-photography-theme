fcc2e36add60f70011faee16f615607a29aa26a6
Ben Marley <benjaminjohnmarley@gmail.com>
2017-11-26 20:03:23 +0000

    Clean indentation

--- header.php
+++ header.php
@@ -12,12 +12,12 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -25,34 +25,10 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bianco_nero' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-
-		<!-- BM CHANGE Remove .site-branding -->
-
-				 <!-- <div class="site-branding">
-		 		 <?php
-		 		  if ( is_front_page() && is_home() ) : ?>
-		 			 <h1 class="site-title"><a href="<?php  echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo(  'name' ); ?></a></h1>
-		 		 <?php  else : ?>
-		 			 <p class="site-title"><a href="<?php  echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo(  'name' ); ?></a></p>
-		 		 <?php
-		 		  endif;
-
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-		endif; ?> -->
-	 <!-- </div> --> <!-- .site-branding -->
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-
-<!-- Div below ensures there is a link to the home-page displayed after the responsive menu hides #primary-menu -->
-
-<div class="home__title">
-	<a class="home__title-link" href="http://localhost/bmphoto/">BEN MARLEY PHOTOGRAPHY</a>
-</div>
-
+			<div class="home__title">
+				<a class="home__title-link" href="http://localhost/bmphoto/">BEN MARLEY PHOTOGRAPHY</a>
+			</div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
