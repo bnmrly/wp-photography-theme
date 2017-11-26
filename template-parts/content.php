54ccc38967bcb10d45a817c88c4aba946f214793
Ben Marley <benjaminjohnmarley@gmail.com>
2017-11-26 20:04:30 +0000

    Remove .entry-header comment

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,25 +11,6 @@
 
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<!-- BM CHANGE Remove entry-header -->
-
-	<!-- <header class="entry-header">
-		<?php
-			 if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			 } else {
-				 the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-
-		 if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-		 	<?php bianco_nero_posted_on(); ?> -->
-		 <!-- </div> --> <!-- .entry-meta -->
-		 <!-- <?php
-	 endif; ?> -->
-	 <!-- </header> --> <!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php
 			the_content( sprintf(
